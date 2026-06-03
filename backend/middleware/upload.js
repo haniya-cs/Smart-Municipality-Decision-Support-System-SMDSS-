982a0dae23b154807291e92b3e8a1e9796a89bff
haniya-cs <atwi2haniya@gmail.com>
2026-06-03 22:47:13 +0300

    Fix Image

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const uploadDir = path.join(__dirname, "..", "uploads");
+    const uploadDir = path.join(__dirname, "uploads");
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir);
     }
